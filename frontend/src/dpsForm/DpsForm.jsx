425bd44c5cd5c957164901210cb1ac52e1e69c60
Aleksander Pres <aleksander.pres98@gmail.com>
2024-04-06 15:23:04 +0200

    v0.1

--- frontend/src/dpsForm/DpsForm.jsx
+++ frontend/src/dpsForm/DpsForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './DpsForm.css'
 
 function DpsForm(props) {
@@ -16,17 +16,43 @@ function DpsForm(props) {
     const [nameSuggestions, setNameSuggestions] = useState(pokemonNamesList);
     const [fastMoveSuggestions, setFastMoveSuggestions] = useState([]);
     const [chargedMoveSuggestions, setChargedMoveSuggestions] = useState([]);
+    const [level, setLevel] = useState("Select Level");
+
+    const [fastMoveSuggestionsList, setFastMoveSuggestionsList] = useState("");
+    const [chargedMoveSuggestionsList, setChargedMoveSuggestionsList] = useState("")
+
+
+    useEffect(() => {
+
+        if (fastMoveSuggestions.length > 0 && chargedMoveSuggestions.length > 0) {
+            setFastMoveSuggestionsList(fastMoveSuggestions.map((move, index) => (
+                <div key={index} className="suggestion" onClick={changeFastMove}>
+                    {move}
+                </div>
+            )));
+
+            setChargedMoveSuggestionsList(chargedMoveSuggestions.map((move, index) => (
+                <div key={index} className="suggestion" onClick={changeChargedMove}>
+                    {move}
+                </div>
+            )));
+        }
+        
+    }, [fastMoveSuggestions, chargedMoveSuggestions]);
 
     function showSuggestions(event) {
         const suggestions = event.target.nextSibling;
         suggestions.style.display = "block";
     }
 
-    function hideSuggestions(event) {
-        console.log(event.target);
+    function showLevels() {
+        const levels = document.getElementById("level-suggestions");
+        levels.style.display = 'block';
+    }
 
-        // const suggestions = event.target.nextSibling;
-        // suggestions.style.display = "none";
+    function hideSuggestions(event) {
+        const levels = document.getElementById("level-suggestions");
+        console.log(event);
     }
 
     function handlePokemonNameChange(event) {
@@ -41,24 +67,80 @@ function DpsForm(props) {
         setNameSuggestions(newSuggestions);
     }
 
+    function handleFastMovecChange(event) {
+        setFastMove(event.target.value);
+    }
+
+    function handleChargedMoveChange(event) {
+        setChargedMove(event.target.value);
+    }
+
     function changePokemonName(event) {
         const suggestions = document.getElementById("name-suggestions");
         const pokemonName = event.target.innerText;
+
+        const pokemon = pokemonData.filter(pokemon => (
+            pokemon[0] === pokemonName
+        ));
+
+        console.log(pokemon[0])
+
+        setFastMove("");
+        setChargedMove("");
+
+        setFastMoveSuggestions(pokemon[0][1]);
+        setChargedMoveSuggestions(pokemon[0][2]);
 
         setPokemonName(pokemonName);
 
         suggestions.style.display = "none";
         
     }
+
+    function changeFastMove(event) {
+        
+        setFastMove(event.target.innerText);
+        
+        const suggestions = document.getElementById("fast-move-suggestions");
+        suggestions.style.display = "none";
+
+    }
+
+    function changeChargedMove(event) {
+
+        setChargedMove(event.target.innerText);
+
+        const suggestions = document.getElementById("charged-move-suggestions");
+        suggestions.style.display = "none";
+
+    }
+
+    function changeLevel(event) {
+
+        setLevel(event.target.innerText);
+
+        const levels = document.getElementById("level-suggestions");
+        levels.style.display = "none";
+
+    }
     
     const nameSuggestionsList = nameSuggestions.map((name, index) => (
         <div key={index} className="suggestion" onClick={changePokemonName}>
             {name}
         </div>
+    ));
+
+    const levels = [];
+    for (let level = 51; level > 0.5; level -= 0.5) {
+        levels.push(level)
+    };
+
+    const levelsList = levels.map((level, index) => (
+        <div key={index} className="level" onClick={changeLevel}>{level}</div>
     ))
 
     return(
-        <div className="form-container">
+        <div className="form-container" onClick={hideSuggestions}>
             <div className="form">
                 <label>Dps Calculator</label><br /><br />
                 <div className="input-container">
@@ -69,14 +151,27 @@ function DpsForm(props) {
                 </div>
 
                 <div className="input-container">
-                    <input type="text" placeholder="Fast Move" onFocus={showSuggestions} onBlur={hideSuggestions}/>
-                    <div className="suggestions"></div><br />
+                    <input type="text" placeholder="Fast Move" value={fastMove} onChange={handleFastMovecChange} onFocus={showSuggestions} />
+                    <div id="fast-move-suggestions" className="suggestions">
+                        {fastMoveSuggestionsList}
+                    </div>
                 </div>
 
                 <div className="input-container">
-                    <input type="text" placeholder="Charged Move" onFocus={showSuggestions} onBlur={hideSuggestions}/>
-                    <div className="suggestions"></div><br />
+                    <input type="text" placeholder="Charged Move" value={chargedMove} onChange={handleChargedMoveChange} onFocus={showSuggestions} />
+                    <div id="charged-move-suggestions" className="suggestions">
+                        {chargedMoveSuggestionsList}
+                    </div>
                 </div>
+
+                <div className="level-select-container">
+                    <div onClick={showLevels}>
+                        <p>{level} &darr;</p>
+                    </div>
+                    <div id="level-suggestions" className="level-suggestions" >
+                        {levelsList}
+                    </div>
+                </div><br />
 
                 <input type="submit" value="Calculate" />
             </div>
